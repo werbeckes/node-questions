7a171a3346fa057af881635e63b1d42d3ac35771
Nathan Werbeckes <werbeckes@adorable.io>
2017-03-18 14:44:00 -0500

    Routing works

--- routes/api/questions.js
+++ routes/api/questions.js
@@ -3,9 +3,4 @@ var models = require('../models');
 
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Working Express' });
-});
-
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var models  = require('../models');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
+});
+
+router.post('/api/users', function(req, res) {
+  models.User.create({
+    username: req.body.username
+  }).then( () => { res.send('created') });
 });
 
 module.exports = router;
