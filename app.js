2ff559fba4d180af55a317f3a6b4f1563aa9cec0
Nathan Werbeckes <werbeckes@adorable.io>
2017-03-20 10:17:11 -0500

    Implements GET and POST for questions model

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/api/users', users);
-app.use('/api/questions', questions);
+app.use('/api', questions);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/api/questions.js
+++ routes/api/questions.js
@@ -2,5 +2,33 @@ var express = require('express');
 var models = require('../../models');
 
 var router = express.Router();
+// TODO: use `router.use`, but not specific to just user_id
+// app.params('user_id', (req, res, next) => {
+//   var user_id = req.params.user_id;
+
+//   if (user_id) {
+//     req.user_id = user_id;
+//     next();
+//   } else {
+//     res.status('422').json('Invalid user_id');
+//   }
+// });
+
+router.get('/users/:user_id/questions', (req, res) => {
+  models.Question.findAll({
+    attributes: ['user_id', 'id', 'text']
+  }).then ( (questions) => {
+    res.status(200).send(questions);
+  });
+});
+
+router.post('/users/:user_id/questions', (req, res) => {
+  models.Question.create({
+    text: req.body.text,
+    user_id: req.params.user_id
+  }).then( () => {
+    res.sendStatus(201);
+  });
+});
 
 module.exports = router;
